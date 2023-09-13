6f94e0387657e7c00906fb97462a662118092710
LALIT KUMAR <lalitkumar76654@gmail.com>
2023-09-13 12:14:47 +0530

    addded navigation and sharred layout for home and starred pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 const Home = () => {
   // return <div>Home page</div>;
   return (
     <div>
-      <Link to="/starred">Go to starred page</Link>
+      {/* <Link to="/starred">Go to starred page</Link> */}
+      Home
     </div>
   );
 };
