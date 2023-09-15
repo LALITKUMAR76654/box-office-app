84b5b5142cc29a439429d28f4404c89403504b69
LALIT KUMAR <lalitkumar76654@gmail.com>
2023-09-15 19:26:21 +0530

    displayed shows and actors data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { searchForShows, searchForPeople } from '../api/tvmaze';
 import SearchForm from '../components/SearchForm';
+import ShowGrid from '../components/shows/ShowGrid';
+import ActorsGrid from '../components/actors/ActorsGrid';
 // import { Link } from 'react-router-dom';
 const Home = () => {
   // return <div>Home page</div>;
@@ -57,12 +59,24 @@ const Home = () => {
       return <div>Error occured:{apiDataError.message}</div>;
     }
 
+    // if (apiData) {
+    //   return apiData[0].show
+    //     ? apiData.map(data => <div key={data.show.id}>{data.show.name}</div>)
+    //     : apiData.map(data => (
+    //         <div key={data.person.id}>{data.person.name}</div>
+    //       ));
+    // }
+
+    if (apiData?.length === 0) {
+      return <div>No results</div>;
+    }
+
     if (apiData) {
-      return apiData[0].show
-        ? apiData.map(data => <div key={data.show.id}>{data.show.name}</div>)
-        : apiData.map(data => (
-            <div key={data.person.id}>{data.person.name}</div>
-          ));
+      return apiData[0].show ? (
+        <ShowGrid shows={apiData} />
+      ) : (
+        <ActorsGrid actors={apiData} />
+      );
     }
 
     return null;
