e45a538a99c4411bd1498ed47f10343fc8ee1396
LALIT KUMAR <lalitkumar76654@gmail.com>
2023-09-16 08:22:50 +0530

    created dynamic show page

---
+++ src/pages/Show.jsx
@@ -0,0 +1,10 @@
+import { useParams } from 'react-router-dom';
+
+const Show = () => {
+  const { showId } = useParams();
+  //   console.log(params);
+
+  return <div>Show page for show {showId}</div>;
+};
+
+export default Show;
