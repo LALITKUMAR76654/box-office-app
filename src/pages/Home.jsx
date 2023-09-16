f261623e9bc506aa61e944d2651569585d73b476
LALIT KUMAR <lalitkumar76654@gmail.com>
2023-09-16 16:16:22 +0530

    used useQuery on Home page to fetch search data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import { searchForShows, searchForPeople } from '../api/tvmaze';
+
 import SearchForm from '../components/SearchForm';
 import ShowGrid from '../components/shows/ShowGrid';
 import ActorsGrid from '../components/actors/ActorsGrid';
@@ -8,47 +10,56 @@ const Home = () => {
   // return <div>Home page</div>;
   // const [inputValue, setInputValue] = useState('');
 
-  const [apiData, setApiData] = useState(null);
-  const [apiDataError, setApiDataError] = useState(null);
+  const [filter, setFilter] = useState('');
+
+  const { data: apiData, error: apiDataError } = useQuery({
+    queryKey: ['search', filter],
+    queryFn: () =>
+      filter.searchOption === 'shows'
+        ? searchForShows(filter.q)
+        : searchForPeople(filter.q),
+    // ⬇️ disabled as long as the filter is empty
+    enabled: !!filter,
+    refetchOnWindowFocus: false,
+  });
+
+  // const [apiData, setApiData] = useState(null);
+  // const [apiDataError, setApiDataError] = useState(null);
 
   // console.log(searchOption);
   // console.log(apiDataError);
   // console.log(inputValue);
 
   const onSearch = async ({ q, searchOption }) => {
+    setFilter({ q, searchOption });
+
     // ev.preventDefault();
-
-    try {
-      setApiDataError(null);
-
-      // if (searchOption === 'shows') {
-      //   const result = await searchForShows(q);
-      //   setApiData(result);
-      // } else {
-      //   const result = await searchForPeople(q);
-      //   setApiData(result);
-      // }
-      let result;
-
-      if (searchOption === 'shows') {
-        result = await searchForShows(q);
-      } else {
-        result = await searchForPeople(q);
-      }
-      setApiData(result);
-    } catch (error) {
-      setApiDataError(error);
-    }
-
+    // try {
+    //   setApiDataError(null);
+    //   // if (searchOption === 'shows') {
+    //   //   const result = await searchForShows(q);
+    //   //   setApiData(result);
+    //   // } else {
+    //   //   const result = await searchForPeople(q);
+    //   //   setApiData(result);
+    //   // }
+    //   let result;
+    //   if (searchOption === 'shows') {
+    //     result = await searchForShows(q);
+    //   } else {
+    //     result = await searchForPeople(q);
+    //   }
+    //   setApiData(result);
+    // } catch (error) {
+    //   setApiDataError(error);
+    // }
     // console.log(apiData);
-
     // const body = await apiGet(`/search/shows?q=${searchStr}`);
     // const response = await fetch(
     //   `https://api.tvmaze.com/search/shows?q=${searchStr}`
     //   // 'https://api.tvmaze.com/search/shows?q=boys'
     // );
     // const body = await response.json();
-
     // console.log(body);
     //     .then(response => response.json())
     //     .then(body => console.log(body));
